2794d836bcd9a0513fe4345c4939f915f6aefb71
scseong <goidtv@naver.com>
2023-11-06 16:09:14 +0900

    refactor: App 컴포넌트에서 상태 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import './App.css';
 
 export default function App() {
-  return <TodoList />;
+  const [todos, setToDos] = useState([]);
+
+  const handleAdd = (inserted) => setToDos([...todos, inserted]);
+  const handleUpdate = (updated) => {
+    updated.isDone = !updated.isDone;
+    setToDos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
+  };
+  const handleDelete = (deleted) => {
+    const result = window.confirm('정말로 삭제하시겠습니까?');
+    if (!result) return;
+
+    setToDos(todos.filter((todo) => todo.id !== deleted.id));
+  };
+
+  return (
+    <main className="container">
+      <h1>"나의 할 일 목록"</h1>
+      <AddTodo onAddTodo={handleAdd} />
+      <TodoList
+        todos={todos}
+        handleUpdate={handleUpdate}
+        handleDelete={handleDelete}
+      />
+    </main>
+  );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,59 +1,39 @@
-import React, { useState } from 'react';
-import AddTodo from './AddTodo';
+import React from 'react';
 import TodoItem from './TodoItem';
 import './TodoList.modules.css';
 
-export default function TodoList() {
-  const [todos, setToDos] = useState([]);
+export default function TodoList({ todos, handleUpdate, handleDelete }) {
   const { doingTodos, doneTodos } = filterTodos(todos);
 
-  const handleAdd = (inserted) => setToDos([...todos, inserted]);
-  const handleUpdate = (updated) => {
-    updated.isDone = !updated.isDone;
-    setToDos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
-  };
-  const handleDelete = (deleted) => {
-    const result = window.confirm('정말로 삭제하시겠습니까?');
-    if (!result) return;
-
-    setToDos(todos.filter((todo) => todo.id !== deleted.id));
-  };
-
   return (
-    <main className="container">
-      <h1>"나의 할 일 목록"</h1>
-      <section>
-        <AddTodo onAddTodo={handleAdd} />
-      </section>
-      <section className="todo">
-        <div className="todo-box">
-          <h3>🔥 진행중</h3>
-          <ul className="todo-list">
-            {doingTodos.map((todo) => (
-              <TodoItem
-                key={todo.id}
-                todo={todo}
-                onDelete={handleDelete}
-                onUpdate={handleUpdate}
-              />
-            ))}
-          </ul>
-        </div>
-        <div className="todo-box">
-          <h3>✅ 완료</h3>
-          <ul className="todo-list">
-            {doneTodos.map((todo) => (
-              <TodoItem
-                key={todo.id}
-                todo={todo}
-                onDelete={handleDelete}
-                onUpdate={handleUpdate}
-              />
-            ))}
-          </ul>
-        </div>
-      </section>
-    </main>
+    <section className="todo">
+      <div className="todo-box">
+        <h3>🔥 진행중</h3>
+        <ul className="todo-list">
+          {doingTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onDelete={handleDelete}
+              onUpdate={handleUpdate}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="todo-box">
+        <h3>✅ 완료</h3>
+        <ul className="todo-list">
+          {doneTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onDelete={handleDelete}
+              onUpdate={handleUpdate}
+            />
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
 
